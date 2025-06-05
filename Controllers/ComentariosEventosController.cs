5d001feb1b61b9c3147f3b067792b0d6560cfa21
Clara Crastechini <ccrastechini@gmail.com>
2025-06-05 16:46:22 -0300

    feat: atualizando api

---
+++ Controllers/ComentariosEventosController.cs
@@ -0,0 +1,137 @@
+﻿using Azure;
+using Azure.AI.ContentSafety;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapi.event_.Contexts;
+using webapi.event_.Domains;
+using webapi.event_.Interfaces;
+
+namespace webapi.event_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ComentariosEventosController : ControllerBase
+    {
+        private readonly IComentariosEventosRepository _comentariosEventosRepository;    
+        private readonly ContentSafetyClient _contentSafetyClient;
+        private readonly Context _context;
+
+        public ComentariosEventosController(ContentSafetyClient contentSafetyClient, IComentariosEventosRepository comentariosEventosRepository, Context contexto)
+        {
+            _comentariosEventosRepository = comentariosEventosRepository;
+            _contentSafetyClient = contentSafetyClient;
+            _context = contexto;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post (ComentariosEventos comentario)
+        {
+            try
+            {
+                Eventos eventoBuscado = _context.Eventos.FirstOrDefault(e => e.IdEvento == comentario.IdEvento);
+
+                if (eventoBuscado == null)
+                {
+                    return NotFound("Evento não encontrado");
+                }
+
+                if(eventoBuscado.DataEvento >= DateTime.UtcNow)
+                {
+                    return BadRequest("Não é possível comentar um evento que ainda não aconteceu");
+                }
+
+
+
+                if (string.IsNullOrEmpty(comentario.Descricao))
+                {
+                    return BadRequest("O texto a ser moderado nao pode estar vazio");
+                }
+
+
+                //criar objeto de analise de content safety
+                var request = new AnalyzeTextOptions(comentario.Descricao);
+            
+                //chamar a API do content safety
+                Response<AnalyzeTextResult> response = await _contentSafetyClient.AnalyzeTextAsync(request);
+
+                //verificar se o texto analisado tem alguma severidade
+                bool temConteudoImproprio = response.Value.CategoriesAnalysis.Any(c => c.Severity > 0);
+      
+                // se o conteudo for improprio, nao exibira, caso contrario, exibe
+                comentario.Exibe = !temConteudoImproprio; 
+
+
+                //cadastra de fato o comentario
+                _comentariosEventosRepository.Cadastrar(comentario);
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                _comentariosEventosRepository.Deletar(id);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("ListarSomenteExibe")]
+        public IActionResult GetExibe(Guid id)
+        {
+            try
+            {
+                return Ok(_comentariosEventosRepository.ListarSomenteExibe(id));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Get(Guid id)
+        {
+            try
+            {
+                return Ok(_comentariosEventosRepository.Listar(id));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet("BuscarPorIdUsuario")]
+        public IActionResult GetByIdUser(Guid idUsuario, Guid idEvento)
+        {
+            try
+            {
+                return Ok(_comentariosEventosRepository.BuscarPorIdUsuario(idUsuario, idEvento));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
